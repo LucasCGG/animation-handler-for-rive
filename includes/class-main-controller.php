a2c8b1416471898678d910f4caa1cc40b67d9485
Lucas Gabriel Colaço <80749221+LucasCGG@users.noreply.github.com>
2024-12-28 21:03:03 +0100

    Update class-main-controller.php

--- includes/class-main-controller.php
+++ includes/class-main-controller.php
@@ -12,11 +12,11 @@ if ( class_exists( 'Elementor\Widget_Base' ) ) {
     class Main_Controller extends Widget_Base {
 
         public function get_name(): string {
-            return 'rive_animation';
+            return 'animation_handler_for_rive';
         }
 
         public function get_title(): string {
-            return __( 'Rive Animation', 'rive-animation-handler' );
+            return __( 'Animation Handler for Rive', 'animation-handler-for-rive' );
         }
 
         public function get_icon(): string {
@@ -31,70 +31,70 @@ if ( class_exists( 'Elementor\Widget_Base' ) ) {
             $this->start_controls_section(
                 'section_content',
                 [
-                    'label' => __( 'Content', 'rive-animation-handler' ),
+                    'label' => __( 'Content', 'animation-handler-for-rive' ),
                 ]
             );
 
             $this->add_control(
                 'canvas_id',
                 [
-                    'label'       => __( 'Canvas ID', 'rive-animation-handler' ),
+                    'label'       => __( 'Canvas ID', 'animation-handler-for-rive' ),
                     'type'        => \Elementor\Controls_Manager::TEXT,
                     'default'     => 'rive-canvas-{unique_id}', // Placeholder for unique ID.
-                    'description' => __( 'The ID of the canvas element where the Rive animation will be rendered. A unique ID will be auto-generated if left with "{unique_id}".', 'rive-animation-handler' ),
+                    'description' => __( 'The ID of the canvas element where the Rive animation will be rendered. A unique ID will be auto-generated if left with "{unique_id}".', 'animation-handler-for-rive' ),
                 ]
             );
 
             $this->add_control(
                 'rive_file',
                 [
-                    'label'       => __( 'Select Rive File', 'rive-animation-handler' ),
+                    'label'       => __( 'Select Rive File', 'animation-handler-for-rive' ),
                     'type'        => \Elementor\Controls_Manager::MEDIA,
                     'media_type'  => 'application/octet-stream', // For .riv files.
                     'default'     => [
                         'url' => '',
                     ],
-                    'description' => __( 'The Rive file to be used for the animation.', 'rive-animation-handler' ),
+                    'description' => __( 'The Rive file to be used for the animation.', 'animation-handler-for-rive' ),
                 ]
             );
 
             $this->add_control(
                 'state_machine',
                 [
-                    'label'       => __( 'State Machine Name', 'rive-animation-handler' ),
+                    'label'       => __( 'State Machine Name', 'animation-handler-for-rive' ),
                     'type'        => \Elementor\Controls_Manager::TEXT,
                     'default'     => 'State Machine 1',
-                    'description' => __( 'The name of the state machine to control the animation.', 'rive-animation-handler' ),
+                    'description' => __( 'The name of the state machine to control the animation.', 'animation-handler-for-rive' ),
                 ]
             );
 
             $this->add_control(
                 'viewport',
                 [
-                    'label'       => __( 'Viewport', 'rive-animation-handler' ),
+                    'label'       => __( 'Viewport', 'animation-handler-for-rive' ),
                     'type'        => \Elementor\Controls_Manager::TEXT,
                     'default'     => 'null',
-                    'description' => __( 'The viewport object.', 'rive-animation-handler' ),
+                    'description' => __( 'The viewport object.', 'animation-handler-for-rive' ),
                 ]
             );
 
             $this->add_control(
                 'threshold',
                 [
-                    'label'       => __( 'Observer Threshold', 'rive-animation-handler' ),
+                    'label'       => __( 'Observer Threshold', 'animation-handler-for-rive' ),
                     'type'        => \Elementor\Controls_Manager::NUMBER,
                     'default'     => 0.7,
                     'step'        => 0.01,
                     'min'         => 0,
                     'max'         => 1,
-                    'description' => __( 'The threshold value for the IntersectionObserver. This parameter determines how much of the canvas element must be visible in the viewport before the animation starts. A value of `0.0` means the animation will start as soon as any part of the element is visible, while a value of `1.0` means the entire element must be visible', 'rive-animation-handler' ),
+                    'description' => __( 'The threshold value for the IntersectionObserver. This parameter determines how much of the canvas element must be visible in the viewport before the animation starts. A value of `0.0` means the animation will start as soon as any part of the element is visible, while a value of `1.0` means the entire element must be visible', 'animation-handler-for-rive' ),
                 ]
             );
 
             $this->add_control(
                 'layout_fit',
                 [
-                    'label'       => __( 'Layout Fit', 'rive-animation-handler' ),
+                    'label'       => __( 'Layout Fit', 'animation-handler-for-rive' ),
                     'type'        => \Elementor\Controls_Manager::SELECT,
                     'default'     => 'contain',
                     'options'     => [
@@ -106,7 +106,7 @@ if ( class_exists( 'Elementor\Widget_Base' ) ) {
                         'none'      => 'None',
                         'scaleDown' => 'Scale Down',
                     ],
-                    'description' => __( 'The layout fit value for the Rive animation.', 'rive-animation-handler' ),
+                    'description' => __( 'The layout fit value for the Rive animation.', 'animation-handler-for-rive' ),
                 ]
             );
             $this->end_controls_section();
@@ -130,14 +130,14 @@ if ( class_exists( 'Elementor\Widget_Base' ) ) {
 
             // Ensure a Rive file is selected before rendering
             if ( empty( $rive_file_url ) ) {
-                return new WP_Error( 'missing_url', __( 'The Rive file URL is required.', 'rive-animation-handler' ) );
+                return new WP_Error( 'missing_url', __( 'The Rive file URL is required.', 'animation-handler-for-rive' ) );
             }
 
             // Render the canvas element
             echo "<canvas id='{$canvas_id}' class='rive-canvas' style='width: 100%; height: 100%;'></canvas>";
 
             // Enqueue necessary JavaScript
-            wp_enqueue_script( 'rive-animation-handler', plugins_url( 'assets/js/rive-handler.js', __FILE__ ), [], '1.0.0', true );
+            wp_enqueue_script( 'animation-handler-for-rive', plugins_url( 'assets/js/rive-handler.js', __FILE__ ), [], '1.0.0', true );
 
             // Inline script to initialize the Rive animation
             $inline_script = "
@@ -168,7 +168,7 @@ if ( class_exists( 'Elementor\Widget_Base' ) ) {
                 });
             ";
 
-            wp_add_inline_script( 'rive-animation-handler', $inline_script );
+            wp_add_inline_script( 'animation-handler-for-rive', $inline_script );
         }
     }
 }
